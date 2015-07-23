b9ce9ed6109d0dfa3518f177bd0e99c28eb5a18f
Mrmaxmeier <Mrmaxmeier@gmail.com>
2015-07-23 18:36:39 +0200

    python wrapper + simpleplayer

--- src/main/python/game_wrapper.py
+++ src/main/python/game_wrapper.py
@@ -1,29 +1,23 @@
 from wrapper import AIWrapper
 
-class Actions:
-	COOPERATE = True
-	BETRAY = False
-
 class GameWrapper(AIWrapper):
-	def update(self, state):
-		cooperate = not not self.ai.step()
+	def update(self, updates):
 		response = {
 			"output": self.output.read(),
-			"response": cooperate
+			"chips": self.ai.einsatz()
 		}
 		return response
 
-	def getState(self, d):
-		if "enemyResponse" in d and "ownResponse" in d:
+	def process(self, d):
+		if all([key in d for key in ["ownWallet", "ownWonChips", "ownChips", "enemyWallet", "enemyWonChips", "enemyChips", ]]):
 			if hasattr(self.ai, "process"):
-				self.ai.process(d["ownResponse"], d["enemyResponse"])
+				self.ai.process(*d.values())
 			else:
 				print("KI verarbeitet Daten aufgrund fehlender 'process' Methode nicht.")
-		return None
 
 	def del_output(self, d):
 		d.pop("output")
-	
+
 	def add_output(self, d, o):
 		d["output"] += o
 

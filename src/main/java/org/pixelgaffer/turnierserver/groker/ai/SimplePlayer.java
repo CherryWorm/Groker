2e8c23339ea4fbc78d40cacb238bd2c18e1dc3ab
nicosio2 <nico.gruendel@web.de>
2015-07-23 13:43:43 +0200

    SimplePlayer hinzugefügt

---
+++ src/main/java/org/pixelgaffer/turnierserver/groker/ai/SimplePlayer.java
@@ -0,0 +1,18 @@
+package org.pixelgaffer.turnierserver.groker.ai;
+
+public class SimplePlayer extends GrokerAi {
+
+    public SimplePlayer(String[] args) {
+	super(args);
+    }
+
+    @Override
+    public int einsatz(AiDaten du, AiDaten gegner) {
+	return 5;
+    }
+    
+    public static void main(String[] args) {
+	new SimplePlayer(args).start();
+    }
+
+}

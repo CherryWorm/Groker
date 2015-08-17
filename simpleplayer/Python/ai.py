7a5570da510a8160baf1f135eb5fdfd8b7884d67
Mrmaxmeier <Mrmaxmeier@gmail.com>
2015-08-18 00:18:23 +0200

    Doku

--- simpleplayer/Python/ai.py
+++ simpleplayer/Python/ai.py
@@ -1,7 +1,22 @@
 import random
+import pprint
 
 class AI:
 	def einsatz(self):
+		"""Diese Funkion wird jede Runde aufgerufen, und gibt die Punkte zurück, die gesetzt werden sollen."""
 		einsatz = random.randrange(1, 6)
 		print("setze:", einsatz)
 		return einsatz
+
+	def process(self, ownWonChips, enemyWonChips, ownChips, enemyChips):
+		"""Diese Funkion wird nach jedem Schritt aufgerufen und ist dafür da, Daten zu verarbeiten.
+		Es werden ownWonChips   (gesammten Chips, die die eigene KI bekommen hat),
+		          enemyWonChips (gesammten Chips, die der gegner bekommen hat),
+		          ownChips      (Chips, die man diese Runde gesetzt hat),
+		          enemyChips    (Chips, die der Gegner diese Runde gesetzt hat) übergeben."""
+		pprint({
+			"ownWonChips": ownWonChips,
+			"enemyWonChips": enemyWonChips,
+			"ownChips": ownChips,
+			"enemyChips": enemyChips
+		})

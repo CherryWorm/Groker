9dbc06581819eefe5ed4a9dcfee17fc540f40a85
Mrmaxmeier <Mrmaxmeier@gmail.com>
2015-09-15 18:21:51 +0200

    weniger Header, jsonloses Protokoll

--- src/main/python/game_wrapper.py
+++ src/main/python/game_wrapper.py
@@ -1,46 +1,23 @@
-####################################################################################
-# game_wrapper.py
-#
-# Copyright (C) 2015 Pixelgaffer
-#
-# This work is free software; you can redistribute it and/or modify it
-# under the terms of the GNU Lesser General Public License as published by the
-# Free Software Foundation; either version 2 of the License, or any later
-# version.
-#
-# This work is distributed in the hope that it will be useful, but without
-# any warranty; without even the implied warranty of merchantability or
-# fitness for a particular purpose. See version 2 and version 3 of the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-####################################################################################
 from wrapper import AIWrapper
 
 class GameWrapper(AIWrapper):
 	def update(self, updates):
 		self.process(updates)
-		response = {
-			"output": self.output.read(),
-			"chips": self.ai.einsatz()
-		}
-		return response
+		return str(self.ai.einsatz()) + ":"
 
 	def process(self, d):
-		if all([key in d for key in ["ownWonChips", "ownChips", "enemyWonChips", "enemyChips"]]):
-			if hasattr(self.ai, "process"):
-				self.ai.process(
-					ownWonChips=d["ownWonChips"],
-					ownChips=d["ownChips"],
-					enemyWonChips=d["enemyWonChips"],
-					enemyChips=d["enemyChips"]
-				)
-			else:
-				print("KI verarbeitet Daten aufgrund fehlender 'process' Methode nicht.")
-
-	def del_output(self, d):
-		d.pop("output")
+		own, their = d.split(";")
+		ownWon, ownChips = own.split(":")
+		enemyWon, enemyChips = enemy.split(":")
+		if hasattr(self.ai, "process"):
+			self.ai.process(
+				ownWonChips=ownWon,
+				ownChips=ownChips,
+				enemyWonChips=enemyWon,
+				enemyChips=enemyChips
+			)
+		else:
+			print("KI verarbeitet Daten aufgrund fehlender 'process' Methode nicht.")
 
 	def add_output(self, d, o):
-		d["output"] += o
+		d += ":" + o
